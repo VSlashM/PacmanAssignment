cf687b7aff10ac4d4188f14aa5e49da810109f60
VSalshM <vito.s.morrow@gmail.com>
2017-01-16 18:04:59 -0500

    First Push
    Push to git

---
+++ PacMan or whatever you wanna call it/Assets/Scripts/CoinRotater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotater : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 rotation = new Vector3 (0, 0, 10);
+		transform.Rotate (rotation);
+	}
+}
---
+++ PacMan or whatever you wanna call it/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Rigidbody pacman;
+	public int score;
+
+    // Use this for initialization
+    void Start() {
+        pacman = GetComponent<Rigidbody>();
+		score = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+       float moveVertical = Input.GetAxis("Vertical");
+       float moveHorizontal = Input.GetAxis("Horizontal");
+
+       Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+       pacman.AddForce(move * speed);
+    }
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Coin")){
+			other.gameObject.SetActive(false);
+			score++;
+		}
+	}
+}
+
