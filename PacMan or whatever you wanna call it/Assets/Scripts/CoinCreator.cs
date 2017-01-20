5605eb97e318e8c4b6d2eeb1d599e2329e82574b
VSalshM <vito.s.morrow@gmail.com>
2017-01-19 19:13:18 -0500

    Teleport; Fixed coin generator
    Added teleport statement to player control script.
    Fix coin creator script to produce randomly generated coins.

---
+++ PacMan or whatever you wanna call it/Assets/Scripts/CoinCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCreator : MonoBehaviour {
+	public Rigidbody coin;
+
+	void Start () {
+		
+		//Vector3 rotation = new Vector3 (90.0f, 90.0f, 45.0f);
+		for (int i = 0; i < 20; i++) {
+			Vector3 pos = new Vector3 (Random.Range (-24f, 24f), 0.6f, Random.Range (-24f, 24f));
+			Rigidbody coinClone = (Rigidbody)Instantiate (coin, pos, transform.rotation);
+		}
+	}	
+
+	void Update () {
+		
+	}
+}
--- PacMan or whatever you wanna call it/Assets/Scripts/PlayerController.cs
+++ PacMan or whatever you wanna call it/Assets/Scripts/PlayerController.cs
@@ -1,31 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
     public Rigidbody pacman;
 	public int score;
+	public Text scoreDisplay;
 
     // Use this for initialization
     void Start() {
         pacman = GetComponent<Rigidbody>();
 		score = 0;
+		scoreDisplay.text = "Score: " + score;
     }
 
-    // Update is called once per frame
     void FixedUpdate() {
        float moveVertical = Input.GetAxis("Vertical");
        float moveHorizontal = Input.GetAxis("Horizontal");
 
        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+		Vector3 teleport = new Vector3 (23f, 0.75f, 23f);
+		Vector3 reset = new Vector3 (-24.0f, 0.75f, -24.0f);
+		if (pacman.transform.position.x <= 25f && pacman.transform.position.x >= 24f &&  pacman.transform.position.z <= 25f && pacman.transform.position.z >= 24f) {
+			pacman.MovePosition(reset);
+		}
        pacman.AddForce(move * speed);
     }
+
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.CompareTag("Coin")){
 			other.gameObject.SetActive(false);
 			score++;
+			scoreDisplay.text = "Score: " + score;
 		}
 	}
 }
