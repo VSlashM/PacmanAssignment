7238f811c14f83e2a61140ba9c5d761751d8b17a
VSalshM <vito.s.morrow@gmail.com>
2017-01-23 17:24:02 -0500

    Added nav mesh, player tracking
    Nav mesh added to level.
    Ghost object has player tracking.

--- PacMan or whatever you wanna call it/Assets/Scripts/GhostController.cs
+++ PacMan or whatever you wanna call it/Assets/Scripts/GhostController.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GhostController : MonoBehaviour {
 	public Rigidbody ghost;
-	public
+	public NavMeshAgent nav;
+	Transform player;
+
+	void Awake(){
+		ghost = GetComponent<Rigidbody> ();
+		nav = GetComponent<NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +20,6 @@ public class GhostController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		nav.SetDestination(player.position);
 	}
 }
